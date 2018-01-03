89ffa65cb16589a3adaa33af7a80e7f2359337bc
Emily Garrison <emily.e.garrison@gmail.com>
2018-01-03 17:11:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+ 
+document.addEventListener('keydown', function(e) {
+  const oneKey = parseInt(e.which);
+  if (oneKey === code[index]) {
+    index++;
+    
+    if (index === alphabet.length) {
+      alert("Congratulations, you've unlocked the Konami Code!");
+    }
+    
+  }
+}) //end parenthases for function(e).
 }
