9309885ff85722a83efcb2673cca33485dfa671f
Emily Garrison <emily.e.garrison@gmail.com>
2018-01-03 17:13:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
+
 
 function init() {
- 
+ let index = 0;
 document.addEventListener('keydown', function(e) {
   const oneKey = parseInt(e.which);
   if (oneKey === code[index]) {
